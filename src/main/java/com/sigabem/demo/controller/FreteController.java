8ed3221fe044b41c05a403c032f5781b2e232a89
José Víctor Ardasse <josevictorps3lima@gmail.com>
2024-05-22 23:32:20 -0300

    feat: documentando funcionalidades para mapeamento no Swagger

--- src/main/java/com/sigabem/demo/controller/FreteController.java
+++ src/main/java/com/sigabem/demo/controller/FreteController.java
@@ -2,6 +2,8 @@ package com.sigabem.demo.controller;
 
 import com.sigabem.demo.entity.Frete;
 import com.sigabem.demo.service.FreteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/frete")
+@Tag(name = "Frete Controller", description = "Controlador para operações de frete")
 public class FreteController {
     @Autowired
     private FreteService freteService;
 
     @PostMapping("/calcular")
+    @Operation(summary = "Calcula o frete", description = "Calcula o valor do frete e a data prevista de entrega com base nos dados fornecidos.")
     public Frete calcularFrete(@RequestBody Frete frete){
         return freteService.calcularFrete(frete);
     }
--- src/main/java/com/sigabem/demo/service/FreteService.java
+++ src/main/java/com/sigabem/demo/service/FreteService.java
@@ -2,6 +2,8 @@ package com.sigabem.demo.service;
 
 import com.sigabem.demo.entity.Frete;
 import com.sigabem.demo.repository.FreteRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 import java.time.LocalDateTime;
 
 @Service
+@Tag(name = "Frete Service", description = "Serviço para cálculo de frete")
 public class FreteService {
 
     @Autowired
@@ -18,6 +21,7 @@ public class FreteService {
 
     private static final String VIACEP_URL = "https://viacep.com.br/ws/%s/json/";
 
+    @Operation(summary = "Calcula o frete", description = "Calcula o valor do frete e a data prevista de entrega com base nos CEPs de origem e destino e o peso da encomenda.")
     public Frete calcularFrete(Frete frete){
         if (frete.getCepOrigem() == null || frete.getCepDestino() == null || frete.getPeso() == null || frete.getNomeDestinatario() == null) {
             throw new IllegalArgumentException("Todos os campos obrigatórios devem ser preenchidos.");
