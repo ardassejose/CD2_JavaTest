68a735248c7d090f1cbe3d07437ca71037782d3a
José Víctor Ardasse <josevictorps3lima@gmail.com>
2024-05-22 23:01:16 -0300

    fix: inserido anotação Repository para a entidade

--- src/main/java/com/sigabem/demo/repository/FreteRepository.java
+++ src/main/java/com/sigabem/demo/repository/FreteRepository.java
@@ -2,6 +2,8 @@ package com.sigabem.demo.repository;
 
 import com.sigabem.demo.entity.Frete;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FreteRepository extends JpaRepository<Frete, Long> {
 }
